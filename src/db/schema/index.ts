0d8e59c197d8125fdd9d12d035f414d617529aa6
Emmanuel Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-07-10 13:08:40 -0300

    Added response generation (#3)
    * feat: add create questions. create room and get room questions
    
    * feat: created the route to receive audios coming from the front-end. and inserted the gemini API KEY
    
    * feat: adds question answer generation based on audio transcripts and updates the question creation path

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,7 +1,11 @@
 // Bare File
 
+import { audioChunks } from './audio-chunks.ts'
+import { questions } from './questions.ts'
 import { rooms } from './rooms.ts'
 
 export const schema = {
   rooms,
+  questions,
+  audioChunks,
 }
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
 import { fastify } from 'fastify'
 import {
   serializerCompiler,
@@ -6,13 +7,19 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
+import { createQuestionsRoute } from './http/routes/create-question.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: 'http://localhost:5173',
 })
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -22,5 +29,9 @@ app.get('/health', () => {
 })
 
 app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestions)
+app.register(createQuestionsRoute)
+app.register(uploadAudioRoute)
 
 app.listen({ port: env.PORT })
