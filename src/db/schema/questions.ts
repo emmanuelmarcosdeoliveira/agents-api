c860b2694c1de9f898f47b940e7a91e7a459eff9
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-07-10 01:26:16 -0300

    feat: created the route to receive audios coming from the front-end. and inserted the gemini API KEY

--- src/db/schema/questions.ts
+++ src/db/schema/questions.ts
@@ -2,7 +2,7 @@ import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
 import { rooms } from './rooms.ts'
 
 export const questions = pgTable('questions', {
-  id: uuid('id').primaryKey().defaultRandom(),
+  id: uuid().primaryKey().defaultRandom(),
   roomId: uuid()
     .references(() => rooms.id)
     .notNull(),
